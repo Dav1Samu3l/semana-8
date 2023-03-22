60604e45116af81bd149545d7c7fecb7a6d11514
Davi Samuel Da Silva <davidev.front@gmail.com>
2023-03-22 10:57:43 -0300

    add e.preventDefault

--- to do list/src/Comonents/CardAdicionar.jsx
+++ to do list/src/Comonents/CardAdicionar.jsx
@@ -1,20 +1,20 @@
 import { useState } from 'react'
 
 
-function CardAdicionar({AdicionarTarefa} ) {
-    const [tarefa, setTarefa] = useState("")
- 
+function CardAdicionar({ AdicionarTarefa }) {
 
+    const [tarefa, setTarefa] = useState("")
 
     return (
         <div>
-            <form>
-                <input type="text"
+            <form  onSubmit={(evento) => evento.preventDefault()}>
+                <input
+                    type="text"
                     placeholder="Adicionar tarefa"
                     value={tarefa}
                     onChange={(e) => setTarefa(e.target.value)} />
             </form>
-            <button onClick={(e) => AdicionarTarefa(tarefa)}>Adicionar</button>
+            <button  type="button" onClick={(e) => AdicionarTarefa(tarefa)}>Adicionar</button>
         </div>
     )
 }
